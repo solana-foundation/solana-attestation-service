adefca948355b5332278e539070cea6c66488c5d
Taylor123 <nftaylor11@gmail.com>
2025-02-13 19:59:30 -0500

    WIP

--- program/src/state/credential.rs
+++ program/src/state/credential.rs
@@ -1,9 +1,10 @@
 extern crate alloc;
 
-use alloc::string::String;
 use alloc::vec::Vec;
 use pinocchio::pubkey::Pubkey;
 use shank::ShankAccount;
+
+use super::VariableLenAccount;
 
 // PDA ["credential", authority, name]
 /// Tracks the authorized signers of for schemas and their attestations.
@@ -14,7 +15,35 @@ pub struct Credential {
     pub authority: Pubkey,
     /// UTF-8 encoded Name of this credential
     /// Includes 4 bytes for length of name
-    pub name: String,
+    pub name: Vec<u8>,
     /// List of signers that are allowed to "attest"
     pub authorized_signers: Vec<Pubkey>,
+}
+
+impl Credential {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut data = Vec::new();
+        // Authority encoding
+        data.extend_from_slice(self.authority.as_ref());
+
+        // Name encoding
+        data.extend_from_slice(&(self.name.len() as u32).to_le_bytes());
+        data.extend_from_slice(self.name.as_ref());
+
+        // Authorized signers encoding
+        data.extend_from_slice(&(self.authorized_signers.len() as u32).to_le_bytes());
+        for signer in &self.authorized_signers {
+            data.extend_from_slice(signer.as_ref());
+        }
+
+        data
+    }
+}
+
+impl VariableLenAccount for Credential {
+    /// Minimum length for Credential account data.
+    /// - 32 authority
+    /// - 4 len of name
+    /// - 4 len of authorized_signers
+    const MIN_LEN: usize = 32 + 4 + 4;
 }
