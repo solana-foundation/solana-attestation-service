ddaa608b93837d65d7d1ec3e7fd6c64ca541e37b
iurage <iurage@protonmail.com>
2025-02-22 23:19:28 +0800

    Add ix

--- program/src/processor/mod.rs
+++ program/src/processor/mod.rs
@@ -1,10 +1,12 @@
 pub mod change_authorized_signers;
+pub mod change_schema_description;
 pub mod change_schema_status;
 pub mod create_credential;
 pub mod create_schema;
 pub mod shared;
 
 pub use change_authorized_signers::*;
+pub use change_schema_description::*;
 pub use change_schema_status::*;
 pub use create_credential::*;
 pub use create_schema::*;
